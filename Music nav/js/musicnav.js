4be282cb4bcf2f2342e03c3e32e8e1a3d543fc79
kellyseo <43875501+kellyseo@users.noreply.github.com>
2018-11-01 19:35:17 -0700

    Add files via upload

---
+++ Music nav/js/musicnav.js
@@ -0,0 +1,37 @@
+// Call this function when the page loads (the jQuery "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
+
+$(".project #myelement").mousemove(changeText);
+function changeText(event) {
+	$(this).text("WTF");
+}
+
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
